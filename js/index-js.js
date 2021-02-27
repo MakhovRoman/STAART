27ec54861d8b9dde3441af42edbca351c90c091e
MakhovRoman <nekromantos30888666@gmail.com>
2021-02-27 12:31:40 +0300

    Desktop once more

--- js/index-js.js
+++ js/index-js.js
@@ -1,21 +1,58 @@
 
-const windowSize = window.matchMedia('(min-width: 480px)');
+const windowSizeMedium  = window.matchMedia('(min-width: 480px)');
 
-function addFormLinks(windowSize) {
+function addFormLinks(windowSizeMedium) {
    let setup            = document.getElementsByClassName('setup')[0];
    let formLinks        = document.createElement('div');
    formLinks.className  = 'formLinks';
    formLinks.innerHTML  = '<a href="#">' + '<img src="images/Tablet/wb-tablete.png" id="Wildberries">' + '</a>' + '<a href="#">' + '<img src="images/Tablet/ozon-tablete.png" id="Ozon">' + '</a>' + '<a href="#">' + '<img src="images/Tablet/yandex-tablete.png" id="Yandex">' + '</a>' + '<a href="#">' + '<img src="images/Tablet/lamod-tablete.png" id="Lamoda">' + '</a>'+ '<a href="#">' + '<img src="images/Tablet/aliexpress-tablete.png" id="Aliexpress">' + '</a>' + '<a href="#">' + '<img src="images/Tablet/goods-tablete.png" id="Goods">' + '</a>' + '<div class="formLinksContacts">' + '<a href="tel:+79452202225">' + '+7 (495) 220-22-25' + '</a>' + '<a href="mailto:hello@staart.ru">' + 'hello@staart.ru' + '</a>' + '</div>';
    let removeFormLinks  = document.getElementsByClassName('formLinks')[0];
-   if (windowSize.matches) {
+   if (windowSizeMedium.matches) {
       setup.insertAdjacentElement("afterbegin", formLinks);
    } else {
       setup.removeChild(removeFormLinks);
    }
 }
 
-addFormLinks(windowSize);
-windowSize.addListener(addFormLinks);
+addFormLinks(windowSizeMedium);
+windowSizeMedium.addListener(addFormLinks);
+
+//перенос ссылок из бургер-меню в хедер
+
+const windowSizeLarge  =   window.matchMedia('(min-width: 1000px');
+
+//перенос элементов бургер-меню в область навигации
+
+function replaceNavLinks(windowSizeLarge) {
+   let burgerBox        =  document.querySelector('ul.burger-box');
+   let insertBurgerBox  =  burgerBox.cloneNode(true);
+   let navMenu          =  document.getElementById('nav');
+   let partner          =  document.getElementById('partner');
+   let removeNavMenu    =  document.querySelector('nav>.burger-box');
+   if (windowSizeLarge.matches) {
+      partner.before(insertBurgerBox);
+   } else {
+      if (removeNavMenu) {
+      removeNavMenu.remove();
+      }
+   }
+}
+replaceNavLinks(windowSizeLarge);
+windowSizeLarge.addListener(replaceNavLinks);
+
+//перенос электронки к телефонному номеру
+
+function replaceEmail (windowSizeLarge) {
+   let phone            =  document.getElementById('phone');
+   let email            =  document.getElementById('email');
+   let insertEmail      =  email.cloneNode(true);
+   if (windowSizeLarge.matches) {
+      phone.after(insertEmail);
+      email.parentNode.remove();
+   }
+}
+replaceEmail(windowSizeLarge);
+windowSizeLarge.addListener(replaceEmail);
 
 //ниже приведен код слайдера
 
